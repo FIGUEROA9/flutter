279ce2d3978fb597b2627f2b3bde2183f1e6c27d
FIGUEROA9 <pf.figueroa9@gmail.com>
2026-09-08 19:59:30 -0500

    ejercicio en clase

---
+++ store_pro_pf/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'screens/login_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'assets/.env');
+  runApp(const StoreProApp());
+}
+
+class StoreProApp extends StatelessWidget {
+  const StoreProApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StorePro Fake API',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
